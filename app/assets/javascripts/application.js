fa8c5018a51fc5e44a404a05b821fa6df8a31b04
Renuka Joshi <renuka.joshi@anchanto.com>
2016-01-20 18:37:50 +0530

    Modified the Assignment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,52 @@
 //= require turbolinks
 //= require_tree .
 //= require.js
+
+
+$(document).ready(function() {
+  $('.has-tooltip').tooltip();
+});
+
+
+           $(document).ready(function () {
+                
+                $('#example1').datepicker({
+                    format: "dd/mm/yyyy"
+                });  
+            
+            });
+
+
+
+ //swal({   title: "Error!",   text: "Here's my error message!",   type: "error",   confirmButtonText: "Cool" });
+
+
+ $('form').validate({
+        rules: {
+            firs_tname: {
+                minlength: 3,
+                maxlength: 15,
+                required: true
+            },
+            last_name: {
+                minlength: 3,
+                maxlength: 15,
+                required: true
+            }
+        },
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if(element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
